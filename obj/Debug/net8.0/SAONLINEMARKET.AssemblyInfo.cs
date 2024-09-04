efde2f8b9989ec2bde90b7d5d5a5fb9e154e97f2
reynardbrits <reynardbrits@gmail.com>
2024-09-04 22:27:08 +0200

    Finally!

--- obj/Debug/net8.0/SAONLINEMARKET.AssemblyInfo.cs
+++ obj/Debug/net8.0/SAONLINEMARKET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAONLINEMARKET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195c1fbf2fc7f71dd13e1ec7067d9098aa0ac631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855bcfd1ac9d4373f7b255865bdbace606b7b5d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAONLINEMARKET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAONLINEMARKET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
